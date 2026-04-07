b8de38839eaa9fade6f98aef2f4e5434f9e9e211
miguelAngel48 <miguelangelponcesnachez@yahoo.es>
2026-04-07 13:40:57 +0200

    first commit for start the proyect

---
+++ src/main/java/com/StarsFighters/StarsFighters/StarsFightersApplication.java
@@ -0,0 +1,13 @@
+package com.StarsFighters.StarsFighters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarsFightersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarsFightersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StarsFighters/StarsFighters/StarsFightersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarsFighters.StarsFighters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarsFightersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
