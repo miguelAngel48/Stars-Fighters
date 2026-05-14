b60aefa47a93a25afc147eb2eb9cba4c7896aa97
JavierHerreraH <javierherreraherssens@gmail.com>
2026-05-14 19:21:59 +0200

    Llamar a fetch cuando aceptamos notificacion

---
+++ frontend/Stars Fighters/src/Components/ProtectedRoute.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/auth/me", {
+            credentials: "include"
+        })
+        .then(res => {
+            if (res.ok) setIsAuthenticated(true);
+            else setIsAuthenticated(false);
+        })
+        .catch(() => setIsAuthenticated(false));
+    }, []);
+
+    if (isAuthenticated === null) return <div>Comprobando acceso...</div>;
+
+    if (isAuthenticated === false) return <Navigate to="/login" replace />;
+
+    return children;
+}
