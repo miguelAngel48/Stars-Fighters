617a6850aedd3b12522f55a680975feaf53e5ba1
JavierHerreraH <javierherreraherssens@gmail.com>
2026-06-13 14:47:31 +0200

    Ultimo

--- src/main/java/com/StarsFighters/StarsFighters/Configuration/SecurityConfig.java
+++ src/main/java/com/StarsFighters/StarsFighters/Configuration/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**", "/api/login/oauth2/**", "/oauth2/**", "/ws-stars/**", "/uploads/**", "/api/characters").permitAll()
                         .anyRequest().authenticated()
