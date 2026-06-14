cc7bd339f584c6036e567e772966ad5d6ae64b87
JavierHerreraH <javierherreraherssens@gmail.com>
2026-06-14 10:59:41 +0200

    Ultimo

--- src/main/java/com/StarsFighters/StarsFighters/Configuration/WebSocketConfig.java
+++ src/main/java/com/StarsFighters/StarsFighters/Configuration/WebSocketConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,17 +20,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(channelInterceptor);
     }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue");
+        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
+        te.setPoolSize(1);
+        te.setThreadNamePrefix("wss-heartbeat-thread-");
+        te.initialize();
+
+        config.enableSimpleBroker("/topic", "/queue")
+                .setHeartbeatValue(new long[]{20000, 20000})
+                .setTaskScheduler(te);
+
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stars").setAllowedOrigins("http://localhost:5173", "http://stars-fighters.z110.alumnes-esliceu.info").withSockJS();
+        registry.addEndpoint("/ws-stars")
+                .setAllowedOrigins(
+                        "http://localhost:5173",
+                        "http://stars-fighters.z110.alumnes-esliceu.info",
+                        "https://stars-fighters.z110.alumnes-esliceu.info"
+                )
+                .withSockJS();
     }
-
-
 }
