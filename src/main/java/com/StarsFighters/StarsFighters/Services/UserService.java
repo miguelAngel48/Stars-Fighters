6c7003b59fb67e871b0a1cddcc7572941b938a82
Javier Herrera <javierherreraherssens@gmail.com>
2026-06-16 09:23:36 +0000

    Update UserService.java

--- src/main/java/com/StarsFighters/StarsFighters/Services/UserService.java
+++ src/main/java/com/StarsFighters/StarsFighters/Services/UserService.java
@@ -99,14 +99,7 @@ public class UserService {
         if (existUser == null) {
             User newUser = new User();
             newUser.setEmail(email);
-            
-            String safeUsername = nombre.replaceAll("\\s+", "").toLowerCase();
-            
-            if (userRepo.findByUsername(safeUsername).isPresent()) {
-                safeUsername = safeUsername + (int)(Math.random() * 1000);
-            }
-            newUser.setUsername(safeUsername);
-
+            newUser.setUsername(nombre);
             newUser.setLevel(1);
             newUser.setRole("USER");
             newUser.setFriendCode(FriendCodeGenerator.generateCode());
